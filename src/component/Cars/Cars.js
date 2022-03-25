f38ab3fa498b3a74aa8c3d97fbcd64a706493540
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-26 03:13:54 +0600

    card grid and css done

--- src/component/Cars/Cars.js
+++ src/component/Cars/Cars.js
@@ -6,13 +6,16 @@ const Cars = (props) => {
     return (
 
         <div className='carsCard'>
-            <img src={image} alt="" className='img-fluid' />
+            <img src={image} alt="" />
 
             <div className='carCard-info'>
                 <h3>{carName}</h3>
                 <h6>Car Model : {carModel}</h6>
                 <h4>{price}</h4>
+                <button className='carsCard-btn'>Add to cart</button>
             </div>
+
+
 
         </div>
 
--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -4,7 +4,7 @@ import './Cart.css'
 const Cart = () => {
     return (
         <div>
-
+            <h1>Choos item</h1>
         </div>
     );
 };
