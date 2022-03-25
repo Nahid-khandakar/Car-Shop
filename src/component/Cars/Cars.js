c9c120e8f5ec347a211e085f96cc7a6b18d7359b
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-26 03:36:11 +0600

    react icon add

--- src/component/Cars/Cars.js
+++ src/component/Cars/Cars.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Cars.css'
+import { FiShoppingCart } from 'react-icons/fi';
 
 const Cars = (props) => {
     const { carName, price, carModel, image } = props.car
@@ -12,7 +13,12 @@ const Cars = (props) => {
                 <h3>{carName}</h3>
                 <h6>Car Model : {carModel}</h6>
                 <h4>{price}</h4>
-                <button className='carsCard-btn'>Add to cart</button>
+
+                {/* react icon use here */}
+                <button className='carsCard-btn'>
+                    <span className='cars-icon'>< FiShoppingCart /></span> Add to cart
+
+                </button>
             </div>
 
 
